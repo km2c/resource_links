d06a238c7a62e699e5b271c4d2135d6f6e3b4a18
Kirk McCutcheon <kirkmccutcheon@gmail.com>
2013-01-15 11:24:49 -0500

    Awesomeness!

---
+++ resource_links/popup.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('.link').on('click', function(){
+    window.open(this.href);
+  });
+});
